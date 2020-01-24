ee9842f72aefab59732560630268328ede1c8ce4
sorianog <13261711+sorianog@users.noreply.github.com>
2020-01-24 09:25:07 -0600

    Completed Lambdas Koan

---
+++ src/playground/Lambdas.kt
@@ -0,0 +1,12 @@
+package playground
+/*
+* Lambdas
+* https://play.kotlinlang.org/koans/Introduction/Lambdas/Task.kt
+*/
+
+fun main () {
+    println("***KOAN: ${containsEven(listOf(0, 2, 3))}")
+    println("***KOAN: ${containsEven(listOf(-1, 1, 3))}")
+}
+
+fun containsEven(collection: Collection<Int>): Boolean = collection.any { it % 2 == 0 }
