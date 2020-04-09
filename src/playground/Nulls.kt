dc93204aa9b2f59d29fae5fb061faa710ba48276
sorianog <13261711+sorianog@users.noreply.github.com>
2020-04-09 13:44:32 -0400

    Finished Nulls Koan

---
+++ src/playground/Nulls.kt
@@ -0,0 +1,30 @@
+package playground
+
+/*
+* Nullable types
+* https://play.kotlinlang.org/koans/Introduction/Nullable%20types/Task.kt
+*/
+
+fun main() {
+    val mailer = TestMailer()
+    sendMessageToClient(Client(null), null, mailer)
+    sendMessageToClient(Client(PersonalInfo("test@email.com")), "Test message", mailer)
+}
+
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    val email = client?.personalInfo?.email
+    if (email != null && message != null) mailer.sendMessage(email, message);
+}
+
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+class TestMailer : Mailer {
+    override fun sendMessage(email: String, message: String) {
+        print("Message sent!\nEmail: $email\nMessage: $message");
+    }
+}
