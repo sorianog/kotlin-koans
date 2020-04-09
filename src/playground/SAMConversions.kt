17aca0041020cce7ca270fda9a7ab77d16f851c0
sorianog <13261711+sorianog@users.noreply.github.com>
2020-04-09 14:34:34 -0400

    Finished SAM conversions Koan

---
+++ src/playground/SAMConversions.kt
@@ -0,0 +1,22 @@
+package playground
+
+import java.util.*
+
+/*
+* SAM conversions
+* https://play.kotlinlang.org/koans/Introduction/SAM%20conversions/Task.kt
+*/
+
+fun main() {
+    print("ArrayList Contents: ")
+    getListSAM().forEach { print("$it ") }
+}
+
+fun getListSAM(): List<Int> {
+    val arrayList = arrayListOf(1, 5, 2)
+    Collections.sort(arrayList, { x, y -> y - x })
+    // Java Collections static method call should be replaced with Kotlin stdlib
+    // Can be simplified to:
+    // arrayList.sortWith(Comparator { x, y -> y - x })
+    return arrayList
+}
